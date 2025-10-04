610a1ccb8625adc6a80bb13c204974516b8d8299
Mikemupararano <kudath@yahoo.co.uk>
2025-10-04 21:47:06 +0100

    Modified post_detail view to take the year, month, day and post arguments and retrieve a published post with slug and publication date.

--- ndikiye_blog/blog/views.py
+++ ndikiye_blog/blog/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import get_object_or_404,render
 from .models import Post
 # from django.http import Http404
 # Create your views here.
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
         id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
     # try:
     #     post = Post.published.get(id=id)
