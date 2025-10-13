774c5a6ab3e2504af6c721a2c5fe05d9c2f43eee
Mikemupararano <kudath@yahoo.co.uk>
2025-10-13 19:43:03 +0100

    Implemented the view to manage the submission of comments and their corresponding URL.

--- ndikiye_blog/blog/urls.py
+++ ndikiye_blog/blog/urls.py
@@ -15,5 +15,8 @@ urlpatterns = [
         name='post_detail'
     ),
     path('post/<int:post_id>/share/', views.post_share, name='post_share'),
+    path(
+        'post/<int:post_id>/comment/', views.post_comment, name='post_comment'
+    ),
     # path('index/', views.index, name='index'),
 ]
