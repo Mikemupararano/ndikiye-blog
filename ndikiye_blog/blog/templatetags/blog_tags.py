9a87ba91efdab443e4221aedf223842b7b875685
Mikemupararano <kudath@yahoo.co.uk>
2025-10-15 21:30:09 +0100

    Added code to blog_tags.py to display the latest 3 posts.

--- ndikiye_blog/blog/templatetags/blog_tags.py
+++ ndikiye_blog/blog/templatetags/blog_tags.py
@@ -1,9 +1,16 @@
 from django import template
 from ..models import Post
-# Create a custom template tag to get the total number of published posts
-# register = template.Library()
+
+# Create a custom template tag library
+register = template.Library()
+
+@register.simple_tag
+def total_posts():
+    """Return total number of published posts."""
+    return Post.published.count()
+
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
+    """Return latest published posts."""
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
-
