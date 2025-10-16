2cad8a33bd70c160e2264e82ecd8fd2d299041aa
Mikemupararano <kudath@yahoo.co.uk>
2025-10-16 19:14:08 +0100

    Defined a feed by subclassing the feedclassof the syndication framework.

--- ndikiye_blog/ndikye_blog/urls.py
+++ ndikiye_blog/ndikye_blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
-# from django.views.generic import RedirectView
+from django.views.generic import RedirectView
 from blog.sitemaps import PostSitemap
 
 sitemaps = {
@@ -16,6 +16,6 @@ urlpatterns = [
         sitemap,
         {'sitemaps': sitemaps},
         name='django.contrib.sitemaps.views.sitemap',
-    )
-    # path('', RedirectView.as_view(pattern_name='blog:post_list', permanent=False)),  # root → blog list
+    ),
+     path('', RedirectView.as_view(pattern_name='blog:post_list', permanent=False)),  # root → blog list
 ]
