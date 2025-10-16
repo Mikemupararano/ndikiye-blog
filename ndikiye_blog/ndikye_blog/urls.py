bae719a4aa80aefe3be29210887e760197d7ceae
Mikemupararano <kudath@yahoo.co.uk>
2025-10-16 16:40:59 +0100

    Defined required imports for sitemaps and defined a sitemaps dictionary.

--- ndikiye_blog/ndikye_blog/urls.py
+++ ndikiye_blog/ndikye_blog/urls.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
-from django.views.generic import RedirectView
+# from django.views.generic import RedirectView
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),  # include app urls
-    path('', RedirectView.as_view(pattern_name='blog:post_list', permanent=False)),  # root → blog list
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap',
+    )
+    # path('', RedirectView.as_view(pattern_name='blog:post_list', permanent=False)),  # root → blog list
 ]
